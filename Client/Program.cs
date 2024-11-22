15a6a7593f050ce432205dd00e54ea09bebdc883
rootIV <vitordevcontato@gmail.com>
2024-11-21 21:14:37 -0300

    feat: first commit (client-server working)
    Signed-off-by: rootIV <vitordevcontato@gmail.com>

---
+++ Client/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), 1999);
+        socket.Connect(endPoint);
+
+        Console.Write("Enter Message: ");
+
+        string msg = Console.ReadLine();
+        byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+        socket.Send(msgBuffer, 0, msgBuffer.Length, 0);
+
+        byte[] buffer = new byte[255];
+        int rec = socket.Receive(buffer, 0, buffer.Length, 0);
+
+        Array.Resize(ref buffer, rec);
+
+        string response = Encoding.Default.GetString(buffer);
+        Console.WriteLine("Server response: {0}", response);
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+
+        Console.ReadKey();
+        //string serverAddress = "127.0.0.1";
+        //int port = 5000;
+
+        //while (true)
+        //{
+        //    Console.WriteLine("Conectando ao servidor...");
+        //    using (TcpClient client = new(serverAddress, port))
+        //    {
+        //        Console.WriteLine("Conectado!");
+
+        //        NetworkStream stream = client.GetStream();
+
+        //        Console.Write("Digite uma mensagem para enviar ao servidor (ou 'exit' para sair): ");
+        //        string message = Console.ReadLine();
+
+        //        if (message.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+        //        {
+        //            break;
+        //        }
+
+        //        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        //        stream.Write(messageBytes, 0, messageBytes.Length);
+
+        //        byte[] buffer = new byte[1024];
+        //        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        //        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        //        Console.WriteLine($"Resposta do servidor: {response}");
+        //    }
+
+        //}
+
+        //Console.WriteLine("Cliente encerrado.");
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        socket.Bind(new IPEndPoint(0, 1999));
+        socket.Listen(0);
+
+        Console.WriteLine("Aguardando conexão...");
+
+        Socket acc = socket.Accept();
+        Console.WriteLine("Cliente conectado.");
+
+        byte[] buffer = new byte[4096];
+        int rec = acc.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+        Array.Resize(ref buffer, rec);
+
+        string receivedMessage = Encoding.Default.GetString(buffer);
+        Console.WriteLine("Received: {0}", receivedMessage);
+
+        string response = "Message received.";
+        byte[] responseBuffer = Encoding.Default.GetBytes(response);
+        acc.Send(responseBuffer, 0, responseBuffer.Length, SocketFlags.None);
+
+        acc.Shutdown(SocketShutdown.Both);
+        acc.Close();
+        socket.Close();
+
+        Console.ReadKey();
+
+        //int port = 5000;
+        //TcpListener server = new TcpListener(IPAddress.Any, port);
+
+        //Console.WriteLine("Iniciando o servidor...");
+        //server.Start();
+        //Console.WriteLine($"Servidor escutando na porta {port}...");
+
+        //while (true)
+        //{
+        //    Console.WriteLine("Aguardando conexão...");
+        //    TcpClient client = server.AcceptTcpClient();
+        //    Console.WriteLine("Cliente conectado!");
+
+        //    NetworkStream stream = client.GetStream();
+
+        //    byte[] buffer = new byte[1024];
+        //    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        //    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        //    Console.WriteLine($"Mensagem recebida: {message}");
+
+        //    string response = "Mensagem recebida com sucesso!";
+        //    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        //    stream.Write(responseBytes, 0, responseBytes.Length);
+
+        //    client.Close();
+        //}
+    }
+}
